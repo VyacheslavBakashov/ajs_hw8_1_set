9f66e1e3f325a6858aa1068c5eddac1684d95eb3
Vyacheslav Bakashov <bakashov@bk.ru>
2023-06-10 15:00:56 +0300

    fix eslint errors

--- src/js/__tests__/team.test.js
+++ src/js/__tests__/team.test.js
@@ -2,23 +2,23 @@ import Team from '../team';
 
 const character1 = {
   name: 'new1',
-  type: 'Magician'
+  type: 'Magician',
 };
 
 const character2 = {
   name: 'new2',
-  type: 'Bowman'
+  type: 'Bowman',
 };
 
 const character3 = {
   name: 'new3',
-  type: 'Daemon'
+  type: 'Daemon',
 };
 
 test('Successful character adding', () => {
   const inp = new Team();
   inp.add(character1);
-  const expected = {members: new Set([character1])};
+  const expected = { members: new Set([character1]) };
   expect(inp).toEqual(expected);
 });
 
@@ -31,7 +31,7 @@ test('Unsuccessful character adding', () => {
 test('Characters array adding', () => {
   const inp = new Team();
   inp.addAll(character1, character2, character3, character1);
-  const expected = {members: new Set([character1, character2, character3])};
+  const expected = { members: new Set([character1, character2, character3]) };
   expect(inp).toEqual(expected);
 });
 
